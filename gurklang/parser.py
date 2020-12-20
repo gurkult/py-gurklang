c8b905d082c64cd5597c6ff2e3fc142b130c41c7
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-20 19:39:41 +0300

    Refactor parser.py to use proper types

--- gurklang/parser.py
+++ gurklang/parser.py
@@ -1,3 +1,11 @@
+from typing import Any, Iterator, List, Literal, NamedTuple, Union
+from gurklang.types import (
+    Instruction,
+    Put, PutCode, Call,
+
+    Value,
+    Atom, Str, Int, Vec, Code, NativeFunction
+)
 import ast
 import itertools
 import re
@@ -17,80 +25,81 @@ TOKEN_RE = re.compile(r"""
 """, re.M | re.X)
 
 
-# _ATOM_CACHE = {}
-# _ATOM_ID = 1
-
-# def atom_id(atom: str):
-#     global _ATOM_ID
-#     if atom not in _ATOM_CACHE:
-#         _ATOM_CACHE[atom] = _ATOM_ID
-#         _ATOM_ID += 1
-#     return _ATOM_CACHE[atom]
+class Token(NamedTuple):
+    name: Literal["COMMENT", "LPAR", "RPAR", "LBR", "RBR", "INT", "STR_D", "STR_S", "ATOM", "NAME"]
+    value: str
+    position: int
 
 
-def _lex(source: str):
+def _lex(source: str) -> Iterator[Token]:
     for m in TOKEN_RE.finditer(source):
         name, value = next(
             (name, value) for (name, value) in m.groupdict().items()
             if value is not None
         )
         if name != "COMMENT":
-            yield (name, value, m.start())
+            yield Token(name, value, m.start())  # type: ignore
 
 
-def _parse_tuple(token_stream):
-    values = []
-
-    for name, value, pos in token_stream:
-        if name == "RPAR":
+def _parse_vec_inner(token_stream: Iterator[Token]) -> Iterator[Value]:
+    for token in token_stream:
+        if token.name == "RPAR":
             break
 
-        if name not in ("NAME", "INT", "STR_D", "STR_S"):
-            raise ValueError((name, value, pos))
+        if token.name not in ("NAME", "INT", "STR_D", "STR_S"):
+            raise ValueError(token)
 
-        if name == "INT":
-            values.append(("int", int(value)))
-        elif name == "NAME":
-            values.append(("atom", value))
+        if token.name == "INT":
+            yield Int(int(token.value))
+        elif token.name == "NAME":
+            yield Atom(token.value)
         else:
-            values.append(("str", ast.literal_eval(value)))
+            yield Str(ast.literal_eval(token.value))
     else:
-        raise ValueError((name, value, pos))
-
-    return ("tuple", values)
+        raise ValueError(token)  # type: ignore
 
 
-def _parse_codeblock(token_stream) -> list:
-    instructions = []
-    for name, value, pos in token_stream:
-        if name == "RBR":
+def _parse_vec(token_stream: Iterator[Token]) -> Vec:
+    return Vec(list(_parse_vec_inner(token_stream)))
+
+
+def _parse_codeblock(token_stream: Iterator[Token]) -> Iterator[Instruction]:
+    for token in token_stream:
+        if token.name == "RBR":
             break
-        elif name == "LBR":
-            instr = ("put_code",_parse_codeblock(token_stream))
-        elif name == "LPAR":
-            instr = ("put", _parse_tuple(token_stream))
-        elif name == "INT":
-            instr = ("put", ("int", int(value)))
-        elif name == "NAME":
-            instr =("call", value)
-        elif name == "ATOM":
-            instr = ("put", ("atom", value[1:]))
-        elif name in ("STR_D", "STR_S"):
-            instr = ("put", ("str", ast.literal_eval(value)))
+
+        elif token.name == "LBR":
+            # put_code is distinct from put, because the runtime
+            # will attach a closure to the code
+            yield PutCode(list(_parse_codeblock(token_stream)))
+
+        elif token.name == "LPAR":
+            yield Put(_parse_vec(token_stream))
+
+        elif token.name == "INT":
+            yield Put(Int(int(token.value)))
+
+        elif token.name == "NAME":
+            yield Call(token.value)
+
+        elif token.name == "ATOM":
+            yield Put(Atom(token.value[1:]))
+
+        elif token.name in ("STR_D", "STR_S"):
+            yield Put(Str(ast.literal_eval(token.value)))
+
         else:
-            raise ValueError((name, value, pos))
-        instructions.append(instr)
+            raise ValueError(token)
     else:
-        raise ValueError((name, value, pos))
-    return instructions
+        raise ValueError(token)  # type: ignore
 
 
-def _parse_stream(token_stream, length):
+def _parse_stream(token_stream: Iterator[Token], length: int) -> Iterator[Instruction]:
     return _parse_codeblock(itertools.chain(
         token_stream,
-        [("RBR", "}", length)],
+        [Token("RBR", "}", length)],
     ))
 
 
-def parse(source: str):
-    return _parse_stream(_lex(source), len(source))
+def parse(source: str) -> list[Instruction]:
+    return list(_parse_stream(_lex(source), len(source)))
