92f787d92a6049b88b27b7f72bf6b2cfe00eae55
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-20 21:00:28 +0300

    Refactor vm.py to use proper types

--- gurklang/vm.py
+++ gurklang/vm.py
@@ -1,5 +1,6 @@
-import pprint
 from immutables import Map
+from typing import Any, Callable, Iterator, NoReturn, Optional, TypeVar
+from gurklang.types import Call, Int, NativeFunction, Put, Scope, Stack, Str, Value, Instruction, Code
 
 
 _SCOPE_ID = 0
@@ -9,257 +10,192 @@ def generate_scope_id():
     return _SCOPE_ID
 
 
-# print = lambda *_: None
+def make_scope(parent: Optional[Scope]) -> Scope:
+    return Scope(parent, generate_scope_id(), Map())
 
 
-def join_scopes(main_scope, closure_scope, indent=""):
-    """
-    Update the closure scope with the new changes
-    """
-    print = print_scope = lambda *_, **__: None
-    print(indent + "join_scopes")
-    if main_scope:
-        print_scope(main_scope, indent+"    ")
-    else:
-        print(indent + "main_scope is None")
-    if closure_scope:
-        print_scope(closure_scope, indent+"    ")
-    else:
-        print(indent + "closure_scope is None")
-    print(indent + "----------------\n")
+def call(stack: Stack, scope: Scope, function: Value) -> tuple[Stack, Scope]:
+    if function.tag == "native":
+        return function.fn(stack, scope)
 
-    if main_scope is None:
-        return closure_scope
-    if closure_scope is None:
-        return main_scope
+    elif function.tag == "code":
+        local_scope = make_scope(parent=scope.join_closure_scope(function.closure))
 
-    if main_scope["(id)"] == closure_scope["(id)"]:
-        return main_scope
-
-    else:
-        result = closure_scope.set(
-            "(parent)",
-            join_scopes(main_scope, closure_scope["(parent)"], indent+"    ")
-        )
-        print("Result:")
-        print_scope(result, indent+">>>> ")
-        return result
-
-
-def look_up_name(scope, name):
-    if name in scope:
-        return scope[name]
-    elif scope["(parent)"]:
-        return look_up_name(scope["(parent)"], name)
-    else:
-        raise KeyError(name)
-
-
-def make_scope(parent):
-    return Map({"(parent)": parent, "(id)": generate_scope_id()})
-
-
-def call(stack, scope, function):
-    tag, arg = function
-    if tag == "native":
-        new_stack, new_scope = arg(stack, scope)
-        return new_stack, new_scope
-    elif tag == "code":
-        (instructions, closure) = arg
-        if closure is None:
-            local_scope = make_scope(scope)
-        else:
-            local_scope = make_scope(join_scopes(scope, closure))
-        for instruction in instructions:
+        for instruction in function.instructions:
             stack, local_scope = execute(stack, local_scope, instruction)
-        return stack, local_scope["(parent)"]
+
+        assert local_scope.parent is not None
+        return stack, local_scope.parent
+
     else:
         raise RuntimeError(function)
 
 
-def execute(stack, scope, instruction):
-    tag, arg = instruction
-    if tag == "put":
-        return (arg, stack), scope
-    elif tag == "put_code":
-        return (("code", (arg, scope)), stack), scope
-    elif tag == "call":
-        fn = look_up_name(scope, arg)
-        new_stack, new_scope = call(stack, scope, fn)
-        return new_stack, new_scope
+def execute(stack: Stack, scope: Scope, instruction: Instruction) -> tuple[Stack, Scope]:
+    if instruction.tag == "put":
+        return (instruction.value, stack), scope
+
+    elif instruction.tag == "put_code":
+        return (Code(instruction.value, scope), stack), scope
+
+    elif instruction.tag == "call":
+        return call(stack, scope, scope[instruction.function_name])
+
     else:
         raise RuntimeError(instruction)
 
 
-def _str_value(v):
-    tag, arg = v
-    if tag == "str":
-        return arg
-    elif tag == "int":
-        return str(arg)
-    elif tag == "atom":
-        return ":" + arg
-    elif tag == "code":
+def _stringify_value(v: Value):
+    if v.tag == "str":
+        return v.value
+    elif v.tag == "int":
+        return str(v.value)
+    elif v.tag == "atom":
+        return ":" + v.value
+    elif v.tag == "code":
         return "{...}"
-    elif tag == "tuple":
-        return "(" + ",".join(map(_str_value, arg)) + ")"
-    elif tag == "native":
-        return f"<builtin {arg.__name__}>"
+    elif v.tag == "vec":
+        return "(" + ",".join(map(_stringify_value, v.values)) + ")"
+    elif v.tag == "native":
+        return f"<builtin {v.fn.__name__}>"
     else:
-        return "!!WTF!!"
+        raise RuntimeError(v)
 
 
-def make_builtins():
+def make_builtins(scope_id: int):
     """
     Create the built-in scope populated with built-in functions
     """
     builtins_dict = {}
 
-    def fail(name, reason, stack, scope):
+    def fail(name: str, reason: str, stack: Stack, scope: Scope):
         print(f"Failure in function {name!r}.")
         print("Reason:", reason)
         print("> Stack: ", repr_stack(stack))
-        print("> Most inner scope: ", scope.delete("(parent)"))
-        print("> Parent scope: ", scope["(parent)"].delete("(parent)"))
+        print("> Most inner scope: ", scope.values)
+        if scope.parent is not None:
+            print("> Parent scope: ", scope.parent.values)
         raise RuntimeError(name, reason)
 
-    def register(name=None):
-        def inner(fn):
+    Fail = Callable[[str], NoReturn]
+
+    # Z is contravariant because a function should accept a subset of
+    # stacks (e.g. only stacks with at least 2 elements)
+    Z = TypeVar("Z", bound=Stack, contravariant=True)
+
+    def register(name: Optional[str] = None):
+        def inner(fn: Callable[[Z, Scope, Fail], tuple[Stack, Scope]]):
             fn_name = name or fn.__name__
             def new_fn(stack, scope):
-                local_fail = lambda reason: fail(fn_name, reason, stack, scope)
+                local_fail: Fail = lambda reason: fail(fn_name, reason, stack, scope)
                 try:
                     return fn(stack, scope, local_fail)
                 except Exception as e:
                     local_fail(f"uncaught exception {type(e)}: {' '.join(e.args)}")
             new_fn.__qualname__ = "new_fn"
-            builtins_dict[name or fn.__name__] = ("native", new_fn)
+            builtins_dict[name or fn.__name__] = NativeFunction(new_fn)
         return inner
 
     ###
 
+    # Shortcuts for brevity
+    T, V, S = tuple, Value, Stack
+
     @register()
-    def dup(stack, scope, fail):
+    def dup(stack: T[V, S], scope: Scope, fail: Fail):
         (x, rest) = stack
         return (x, (x, rest)), scope
 
     @register()
-    def swap(stack, scope, fail):
+    def swap(stack: T[V, T[V, S]], scope: Scope, fail: Fail):
         (x, (y, rest)) = stack
         return (y, (x, rest)), scope
 
     @register()
-    def rot3(stack, scope, fail):
+    def rot3(stack: T[V, T[V, T[V, S]]], scope: Scope, fail: Fail):
         (z, (y, (x, rest))) = stack
         return (x, (y, (z, rest))), scope
 
     @register()
-    def jar(stack, scope, fail):
+    def jar(stack: T[V, T[V, S]], scope: Scope, fail: Fail):
         """
         Store a function by a name
         """
-        (name_v, (code_v, rest)) = stack
-        (name_tag, name) = name_v
-        (code_tag, code) = code_v
-        if name_tag != "atom":
-            fail(f"{name_v} is not an atom")
-        if code_tag not in ("code", "native"):
-            fail(f"{code_v} is not code")
-        return rest, scope.set(name, code_v)
+        (identifier, (code, rest)) = stack
+        if identifier.tag != "atom":
+            fail(f"{identifier} is not an atom")
+        if code.tag != "code" and code.tag != "native":
+            fail(f"{code} is not code")
+        return rest, scope.with_member(identifier.value, code)
 
     @register()
-    def var(stack, scope, fail):
+    def var(stack: T[V, T[V, S]], scope: Scope, fail: Fail):
         """
         Store a value by a name
         """
-        (name_v, (value, rest)) = stack
-        (name_tag, name) = name_v
-        if name_tag != "atom":
-            fail(f"{name_v} is not an atom")
-        fn = ("code", ([("put", value)], scope))
-        return rest, scope.set(name, fn)
+        (identifier, (value, rest)) = stack
+        if identifier.tag != "atom":
+            fail(f"{identifier} is not an atom")
+        fn = Code([Put(value)], closure=scope)
+        return rest, scope.with_member(identifier.value, fn)
 
     @register()
-    def print_string(stack, scope, fail):
-        ([tag, arg], rest) = stack
-        if tag != "str":
-            fail(f"{(tag, arg)} is not a string")
-        print(arg)
+    def print_string(stack: T[V, S], scope: Scope, fail: Fail):
+        (head, rest) = stack
+        if head.tag != "str":
+            fail(f"{head} is not a string")
+        print(head.value)
         return rest, scope
 
     @register("str")
-    def str_(stack, scope, fail):
+    def str_(stack: T[V, S], scope: Scope, fail: Fail):
         (x, rest) = stack
-        representation = ("str", _str_value(x))
+        representation = Str(_stringify_value(x))
         return (representation, rest), scope
 
     @register("+")
-    def add(stack, scope, fail):
-        ([xt, xv], ([yt, yv], rest)) = stack
-        if xt != "int" or yt != "int":
-            fail("not an integer")
-        return (("int", xv + yv), rest), scope
+    def add(stack: T[V, T[V, S]], scope: Scope, fail: Fail):
+        (x, (y, rest)) = stack
+        if x.tag != "int" or y.tag != "int":
+            fail(f"{x} cannot be added with {y}")
+        return (Int(x.value + y.value), rest), scope
 
     @register("!")
-    def call_(stack, scope, fail):
+    def exclamation_mark(stack: T[V, S], scope: Scope, fail: Fail):
         (function, rest) = stack
         return call(rest, scope, function)
 
-    builtins_dict["print"] = ("code", ([("call", "str"), ("call", "print_string")], None))
+    builtins_dict["print"] = Code([Call("str"), Call("print_string")], closure=None)
 
     ###
+    return Scope(parent=None, id=scope_id, values=Map(builtins_dict))
 
-    return Map(builtins_dict)
 
-
-builtin_scope = make_builtins().set("(parent)", None).set("(id)", generate_scope_id())
+builtin_scope = make_builtins(generate_scope_id())
 global_scope = make_scope(parent=builtin_scope)
 
 
 
-def unwrap_stack(stack):
+def unwrap_stack(stack: Stack) -> Iterator[Value]:
     while stack is not None:
-        x, stack = stack
+        x, stack = stack  # type: ignore
         yield x
 
 
-def repr_stack(stack):
+def repr_stack(stack) -> list[Value]:
     return [*unwrap_stack(stack)][::-1]
 
 
-def repr_scope(scope):
-    d = dict(scope.items())
-    if d["(parent)"] is not None:
-        d["(parent)"] = repr_scope(d["(parent)"])
+def repr_scope(scope: Scope) -> dict[str, Any]:
+    d = dict(scope.values.items())
+    if scope.parent is not None:
+        d["(parent)"] = repr_scope(scope.parent)
     return d
-
-
-def print_scope(scope, indent="", already_printed=Map()):
-    if scope["(id)"] == builtin_scope["(id)"]:
-        print(indent+"Builtin scope.")
-        return
-
-    # if scope["(id)"] == global_scope["(id)"]:
-    #     print(indent+"Global scope.")
-    #     return
-
-    if scope["(id)"] in already_printed:
-        print(indent + f"SCOPE #{scope['(id)']} (already printed)")
-    else:
-        print(indent + f"SCOPE #{scope['(id)']}")
-        for key in scope:
-            if key not in {"(id)", "(parent)"}:
-                print(indent + f"{key} -> {_str_value(scope[key])}")
-        if scope["(parent)"] is None:
-            print(indent + "Root.")
-        else:
-            print(indent + "Parent:")
-            print_scope(scope["(parent)"], indent+"    ", already_printed.set("(id)", 1))
 
 
 def run(instructions):
     return call(
         stack=None,
         scope=global_scope,
-        function=("code", (instructions, None))
+        function=Code(instructions, closure=None)
     )
