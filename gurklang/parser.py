271516526683ac840354834dd80a01ab64109d82
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-20 22:10:22 +0300

    create type-safe Tokenizer class

--- gurklang/parser.py
+++ gurklang/parser.py
@@ -1,5 +1,5 @@
 from typing import Iterator, Literal, NamedTuple
-from gurklang.parser_utils import build_regexp
+from gurklang.parser_utils import build_regexp, build_regexp_source, build_tokenizer
 from gurklang.types import (
     Instruction,
     Put, PutCode, Call,
@@ -11,35 +11,31 @@ import ast
 import itertools
 
 
-TOKEN_RE = build_regexp((
-    ("COMMENT", r"\#.*($|\n)"),
-    ("WHITESPACE", r"\s+"),
-    ("LPAR", r"\("),
-    ("RPAR", r"\)"),
-    ("LBR", r"\{"),
-    ("RBR", r"\}"),
-    ("INT", r"[-+]?(?:0|[1-9]\d*)"),
-    ("STR_D", r'"(?:\\.|[^"])+"'),
-    ("STR_S", r"'(?:\\.|[^'])+'"),
-    ("ATOM", r"\:(?!\d)[^\"'(){}#: \n\t]+"),
-    ("NAME", r"(?!\d)[^\"'(){}#: \n\t]+"),
-))
+
+tokenizer = build_tokenizer(
+    (
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+        ("LBR", r"\{"),
+        ("RBR", r"\}"),
+        ("INT", r"[-+]?(?:0|[1-9]\d*)"),
+        ("STR_D", r'"(?:\\.|[^"])+"'),
+        ("STR_S", r"'(?:\\.|[^'])+'"),
+        ("ATOM", r"\:(?!\d)[^\"'(){}#: \n\t]+"),
+        ("NAME", r"(?!\d)[^\"'(){}#: \n\t]+"),
+    ),
+    ignored_tokens=(
+        ("COMMENT", r"\#.*($|\n)"),
+        ("WHITESPACE", r"\s+"),
+    ),
+)
 
 
-class Token(NamedTuple):
-    name: Literal["COMMENT", "LPAR", "RPAR", "LBR", "RBR", "INT", "STR_D", "STR_S", "ATOM", "NAME"]
-    value: str
-    position: int
+Token = tokenizer.token_type
 
 
 def _lex(source: str) -> Iterator[Token]:
-    for m in TOKEN_RE.finditer(source):
-        name, value = next(
-            (name, value) for (name, value) in m.groupdict().items()
-            if value is not None
-        )
-        if name != "COMMENT" and name != "WHITESPACE":
-            yield Token(name, value, m.start())  # type: ignore
+    return tokenizer.tokenize(source)
 
 
 def _parse_vec_inner(token_stream: Iterator[Token]) -> Iterator[Value]:
@@ -47,15 +43,14 @@ def _parse_vec_inner(token_stream: Iterator[Token]) -> Iterator[Value]:
         if token.name == "RPAR":
             break
 
-        if token.name not in ("NAME", "INT", "STR_D", "STR_S"):
-            raise ValueError(token)
-
         if token.name == "INT":
             yield Int(int(token.value))
         elif token.name == "NAME":
             yield Atom(token.value)
-        else:
+        elif token.name == "STR_D" or token.name == "STR_S":
             yield Str(ast.literal_eval(token.value))
+        else:
+            raise ValueError(token)
     else:
         raise ValueError(token)  # type: ignore
 
@@ -86,7 +81,7 @@ def _parse_codeblock(token_stream: Iterator[Token]) -> Iterator[Instruction]:
         elif token.name == "ATOM":
             yield Put(Atom(token.value[1:]))
 
-        elif token.name in ("STR_D", "STR_S"):
+        elif token.name == "STR_D" or token.name == "STR_S":
             yield Put(Str(ast.literal_eval(token.value)))
 
         else:
--- gurklang/parser_utils.py
+++ gurklang/parser_utils.py
@@ -1,8 +1,58 @@
+from dataclasses import dataclass
 import re
-from typing import Iterable
+from dataclasses import dataclass
+from typing import Collection, Generic, Iterable, Iterator, Pattern, TypeVar
+
+
+TokenName = TypeVar("TokenName", bound=str)
+
+
+@dataclass
+class Token(Generic[TokenName]):
+    name: TokenName
+    value: str
+    position: int
+
+
+@dataclass
+class Tokenizer(Generic[TokenName]):
+    """
+    Tokenizer with enhanced type safety.
+
+    Don't instantiate this class directly, use `build_tokenizer` instead.
+    """
+    pattern: Pattern[str]
+    ignore: Collection[str] = ()
+
+    @property
+    def token_type(self) -> type[Token[TokenName]]:
+        return Token
+
+    def tokenize(self, source: str) -> Iterator[Token[TokenName]]:
+        for match in self.pattern.finditer(source):
+            name, value = next(
+                (name, value) for (name, value) in match.groupdict().items()
+                if value is not None
+            )
+            if name not in self.ignore:
+                yield Token(name, value, match.start())  # type: ignore
+
 
 def build_regexp_source(lookup: Iterable[tuple[str, str]]):
-    return "|".join(f"(?P<{name}>{pattern})" for name, pattern in lookup)
+    return "|".join(f"(?P<{name}>{pattern})" for name, pattern in lookup)  # type:ignore
+
 
 def build_regexp(lookup: Iterable[tuple[str, str]], flags: re.RegexFlag = re.RegexFlag(0)):
     return re.compile(build_regexp_source(lookup), flags)
+
+
+def build_tokenizer(
+    normal_tokens: tuple[tuple[TokenName, str], ...],
+    flags: re.RegexFlag = re.RegexFlag(0),
+    *,
+    ignored_tokens: tuple[tuple[str, str], ...],
+) -> Tokenizer[TokenName]:
+    return Tokenizer(
+        build_regexp(normal_tokens + ignored_tokens, flags),
+        ignore=[name for name, _pattern in ignored_tokens]
+    ) # type: ignore
