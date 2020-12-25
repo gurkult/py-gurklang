e6fa02131fa1edcc46bc164baad9ec60b39b940a
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-25 16:57:49 +0300

    fix push_docs.sh

--- push_docs.sh
+++ push_docs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/26224526/how-to-push-a-git-ignored-folder-to-a-subtree-branch
 
-STATUS="$(git status)"
-
-if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+if [[ "$(git status)" == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i "" 'site/' ./.gitignore
+    sed -i "/site/d" ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix gh-pages master`:gh-pages --force
+    git subtree push --prefix site/ origin gh-pages
     git reset HEAD~
     git checkout .gitignore
 else
