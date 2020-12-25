77f5324f173205aa870ba7d786c6d92beaaec8ec
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-25 07:54:04 +0300

    Create basic parser test with hypothesis

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,13 @@
 import re
 from gurklang.parser_utils import build_regexp
+from gurklang.parser import parse
+from hypothesis import given, infer, assume
+
+
+@given(program=infer)
+def test_any_program_without_parens_brackets_and_colons_is_valid(program: str):
+    assume(set(program) & {"(", ")", "{", "}", ":"} == set())
+    parse(program)
 
 
 def test_build_regexp():
