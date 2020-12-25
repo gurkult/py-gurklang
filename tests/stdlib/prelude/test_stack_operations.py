7505d8e8636545dd96f6843c3d177d166f3e4c8e
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-25 05:49:03 +0300

    add hypothesis

--- tests/stdlib/prelude/test_stack_operations.py
+++ tests/stdlib/prelude/test_stack_operations.py
@@ -1,14 +1,31 @@
-from tests.test_examples import run, number_stack as nums
+from gurklang.types import CallByName, Put, Value
+from hypothesis import infer, given
+from tests.test_examples import run, irun, number_stack as nums
 
 
-def test_dup():
-    assert run('3 dup') == nums(3, 3)
-    assert run('3 4 2dup') == nums(3, 4, 3, 4)
+@given(a=infer)
+def test_dup(a: int):
+    assert run(f'{a} dup') == nums(a, a)
 
 
-def test_drop():
-    assert run('3 4 drop') == nums(3)
-    assert run('3 4 2drop') == nums()
+@given(a=infer)
+def test_dup_value(a: Value):
+    assert irun(Put(a), CallByName("dup")) == (a, (a, None))
+
+
+@given(a=infer, b=infer)
+def test_2dup(a: int, b: int):
+    assert run(f'{a} {b} 2dup') == nums(a, b, a, b)
+
+
+@given(a=infer, b=infer)
+def test_drop(a: int, b: int):
+    assert run(f'{a} {b} drop') == nums(a)
+
+
+@given(a=infer, b=infer, c=infer)
+def test_2drop(a: int, b: int, c: int):
+    assert run(f'{a} {b} {c} 2drop') == nums(a)
 
 
 def test_swap():
@@ -31,7 +48,14 @@ def test_unrot():
     assert run('3 4 5 6 7 8 2unrot') == nums(5, 6, 7, 8, 3, 4)
 
 
-def test_rot_identities():
+@given(a=infer, b=infer, c=infer)
+def test__rot_rot__is__unrot(a: int, b: int, c: int):
     assert run('3 4 5 rot rot') == run('3 4 5 unrot')
-    assert run('3 4 5 rot rot rot') == run('3 4 5 unrot unrot unrot') == nums(3, 4, 5)
-    assert run('3 4 5 rot unrot rot unrot') == nums(3, 4, 5)
+
+@given(a=infer, b=infer, c=infer)
+def test__rot_rot_rot__is__id(a: int, b: int, c: int):
+    assert run(f'{a} {b} {c} rot rot rot') == nums(a, b, c)
+
+@given(a=infer, b=infer, c=infer)
+def test__unrot_unrot_unrot__is__id(a: int, b: int, c: int):
+    assert run(f'{a} {b} {c} unrot unrot unrot') == nums(a, b, c)
