a52f7304629a5df8bd6da8ca836ae9d229383782
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-27 19:01:21 +0300

    Very final github.io fix

--- push_docs.sh
+++ push_docs.sh
@@ -9,7 +9,7 @@ then
     sed -i "/site\/docs/d" ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix site/docs/ master`:gh-pages --force
+    git push origin `git subtree split --prefix site/ master`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
