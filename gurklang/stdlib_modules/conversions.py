8b110022cf24e5ac2a749e4938c5da2f467c57f3
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-02 00:11:16 +0100

    Add list to tuple conversion

--- gurklang/stdlib_modules/conversions.py
+++ gurklang/stdlib_modules/conversions.py
@@ -14,3 +14,22 @@ def tuple_to_list(stack: T[V, S], fail: Fail):
         fail(f"{head} must be a tuple")
     nested = reduce(lambda a, b: Vec([b, a]), reversed(head.values), Vec([]))
     return nested, rest
+
+
+@module.register_simple("list->tuple")
+def list_to_tuple(stack: T[V, S], fail: Fail):
+    head, rest = stack
+    if head.tag != "vec":
+        fail(f"{head} must be a list")
+    el = head.values
+    values = []
+    while len(el) != 0:
+        if len(el) != 2:
+            fail('a list must be composed of 2 long tuples')
+        car, cdr = el
+        if cdr.tag != 'vec':
+            fail('a list must only contain tuples as the second element')
+        values.append(car)
+        el = cdr.values
+
+    return Vec(values), rest
